79b048efc712833a3b514c43172f17dd8e9968fe
Julia Lisec <julialisec12@gmail.com>
2020-09-01 05:22:59 +0200

    created switching tab-border in main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const tabItems = document.quereySelectorAll('.tab-item');
-const tabContentItems = document.quereySelectorAll('.tab-content-item');
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
+  removeBorder();
 
   // Add border to current tab
   this.classList.add('tab-border');
+}
 
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
 // Listen for tab click
